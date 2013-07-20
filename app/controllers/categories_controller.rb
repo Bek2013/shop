4a9efdaec1d5b18521682dfc3622b2560a89d462
Bek <temp@BMacBook-Pro.local>
2013-07-20 11:36:29 +0400

    Исправили ошибку с категориями

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -24,7 +24,7 @@ class CategoriesController < ApplicationController
 
 
   def create
-    @category = Category.new(params[:category].category(:name, :description))
+    @category = Category.new(params[:category].permit(:name, :description))
     if @category.save
       redirect_to new_category_path, notice: 'Category was successfully created.'
     else
@@ -36,7 +36,7 @@ class CategoriesController < ApplicationController
   def update
     @category = Category.find(params[:id])
 
-      if @category.update_attributes(params[:category].category(:name, :description))
+      if @category.update_attributes(params[:category].permit(:name, :description))
         redirect_to @category, notice: 'Category was successfully updated.'
       else
         render action: "edit"
